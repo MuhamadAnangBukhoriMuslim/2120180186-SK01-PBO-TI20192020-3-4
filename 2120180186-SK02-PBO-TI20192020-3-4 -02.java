d94e12100ccaf0b0706445590744102378f95f0b
anang29muehamad@gmail.com <anang029>
2019-10-03 12:52:54 +0700

    Upload Studi Kasus 02

---
+++ 2120180186-SK02-PBO-TI20192020-3-4 -02.java
@@ -0,0 +1,22 @@
+package sinauclass;
+public class kamera {
+     //property
+    String nama="kamera_ku";
+    int lensa;
+    int ISO;
+    int warna;
+    int resolusi;
+    
+    //method
+    void motret(){
+        System.out.println("motret");
+        
+    } 
+    
+      void vidio() {
+        System.out.println ("vidio");
+    }
+    
+}
+
+
